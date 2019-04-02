a5e55d7a5263d7ad0564c2ce7b4466d67dc18984
Daniel Harvey <daniel.harvey@habito.com>
2019-04-02 13:46:36 +0100

    Deriving

--- haskell/src/Deriving.hs
+++ haskell/src/Deriving.hs
@@ -1,3 +1,12 @@
+{-# LANGUAGE DerivingStrategies #-}
 module Deriving where
 
 -- in which we derive some typeclasses using fancy bullshit
+
+data Pets
+  = Dog
+  | Cat
+  | Gerbil
+  deriving stock (Eq, Ord, Show)
+
+-- deriving Stock means built-in typeclasses that Haskell inately understands
