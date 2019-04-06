283e093190a875779125c4cc9bc7795c0ab14981
Daniel Harvey <danieljamesharvey@gmail.com>
2019-04-06 10:48:46 +0100

    Lit function for nat

--- haskell/src/Typelevel.hs
+++ haskell/src/Typelevel.hs
@@ -32,21 +32,50 @@ data Vector (m :: Nat) (a :: *) where
 instance Show a => Show (Vector n a) where
   show VNil         = "VNil"
   show (VCons a as) = "VCons " ++ show a ++ " (" ++ show as ++ ")"
-{-
-instance JSON.ToJSON a => JSON.ToJSON (Vector n a) where
-  toJSON VNil         = JSON.emptyArray
-  toJSON (VCons a as)
-    = mappend (JSON.Array $ V.singleton val) (JSON.toJSON as)
-    where
-      val :: JSON.Value
-      val = JSON.toJSON a
--}
+
+instance Functor (Vector n) where
+  fmap _ VNil         = VNil
+  fmap f (VCons a as) = VCons (f a) (fmap f as)
+
+vectyBoy :: Vector (Succ (Succ (Succ (Succ Zero)))) Int
+vectyBoy = VCons 1 (VCons 2 (VCons 3 (VCons 4 VNil)))
+
+emptyBoy :: Vector Zero Int
+emptyBoy = VNil
+
+-- look at this unreasonable type signature
+longBoy :: Vector (Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ Zero)))))))) Int
+longBoy = vAppend vectyBoy vectyBoy
+
+mappedBoy :: Vector (Succ (Succ (Succ (Succ Zero)))) Int
+mappedBoy = (* 10) <$> vectyBoy
+-- VCons 10 (VCons 20 (VCons 30 (VCons 40 (VNil))))
 
 -- like the nats, we are moving 'a' from the left pile into the right pile
 -- when everything is in the right pile, return it
 -- note we don't ever bother thinking about the Nats
 -- as VCons takes care of incrementing them
-append :: Vector n a -> Vector m a -> Vector (n + m) a
-append VNil x            = x
-append (VCons a rest) as = append rest (VCons a as)
+vAppend :: Vector n a -> Vector m a -> Vector (n + m) a
+vAppend VNil x            = x
+vAppend (VCons a rest) as = vAppend rest (VCons a as)
+
+-- we can guarantee a value from the head function
+-- because it specifically uses Succ in the type signature
+vHead :: Vector (Succ n) a -> a
+vHead (VCons a _) = a
+
+
+a = vHead vectyBoy
+-- a == 1
+
+-- b = vHead emptyBoy
+-- Couldn't match type ‘'Zero’ with ‘'Succ n0’
+--      Expected type: Vector ('Succ n0) Int
+--        Actual type: Vector 'Zero Int
+
+type family (m :: Nat) - (n :: Nat) :: Nat
+
+type family Lit (n :: Nat) :: Nat where
+  Lit Zero = Zero
+  Lit n = Succ (Lit (n - Succ Zero))
 
