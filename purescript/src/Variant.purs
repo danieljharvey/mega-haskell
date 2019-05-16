290813dc81a1ba38ee4e94294a6cd7f1db0a4f5c
Daniel Harvey <danieljamesharvey@gmail.com>
2019-05-16 18:27:49 +0100

    Yes

--- purescript/src/Variant.purs
+++ purescript/src/Variant.purs
@@ -7,6 +7,7 @@ import Data.Traversable (traverse)
 import Effect (Effect)
 import Effect.Console (logShow)
 import Effect.Ref (Ref, new, read, write)
+import Effect.Timer as T
 import Prim.Row (class Cons)
 
 -- here are our first actions
@@ -15,10 +16,14 @@ data Login
   | Logout
   | LoginSuccess
 
+instance hasLabelLogin :: HasLabel Login "login" 
+
 -- here are our second actions
 data Counting
   = Up
   | Down
+
+instance hasLabelCounting :: HasLabel Counting "counting"
 
 -- an example state type
 type State
@@ -27,15 +32,20 @@ type State
     , value     :: Int
     }
 
-loginReducer :: (LiftedAction -> Effect Unit) -> Login -> State -> Effect State
-loginReducer _ (StartLogin _ _) s
-  = pure $ s { loggedIn = false, loggingIn = true }
+loginReducer 
+  :: Reducer LiftedAction Login State 
+loginReducer dispatch (StartLogin _ _) s = do
+  _ <- T.setTimeout 100 do
+    dispatch (liftAction' LoginSuccess)
+  pure $ s { loggedIn = false, loggingIn = true }
+
 loginReducer _ Logout s
   = pure $ s { loggedIn = false, loggingIn = false }
 loginReducer _ LoginSuccess s
   = pure $ s { loggedIn = true, loggingIn = false }
 
-countReducer :: (LiftedAction -> Effect Unit) -> Counting -> State -> Effect State
+countReducer 
+  :: Reducer LiftedAction Counting State 
 countReducer _ Up s
   = pure $ s { value = s.value + 1 }
 countReducer _ Down s
@@ -54,73 +64,12 @@ type LiftedAction
 -- This runs an action through whichever reducer makes sense
 -- @match@ uses exhaustiveness checking so we must check for every type
 myProcess 
-  :: (LiftedAction -> Effect Unit) 
-  -> State 
-  -> LiftedAction 
-  -> Effect State
+  :: CombinedReducer LiftedAction State
 myProcess dispatch s action' =
   match
     { login:    \action -> loginReducer dispatch action s
     , counting: \action -> countReducer dispatch action s
     } action'
-
-
-{-
-processAll 
-  :: forall actionType
-   . actionType -> Effect Unit
-  -> State
-  -> Array actionType
-   -> Effect State
-processAll myDispatch s as
-  = foldM (process myDispatch) s (reverse as)
-  -}
-
-type RunReducers actionType stateType
-  = (actionType -> Effect Unit)
-  -> stateType
-  -> actionType
-  -> Effect stateType
-
-type Listeners stateType
-  = Array (stateType -> Effect Unit)
-
-data Store actionType stateType
-  = Store { dispatch :: actionType -> Effect Unit
-          , getState :: Effect stateType
-          }
-
-createStore 
-  :: forall stateType actionType
-   . stateType
-  -> Listeners stateType
-  -> RunReducers actionType stateType
-  -> Effect (Store actionType stateType)
-createStore state listeners reducers = do
-  stateRef <- new state
-
-  pure $ Store { dispatch: (update stateRef listeners reducers)
-               , getState: (getState stateRef)
-               }
-  
-update 
-  :: forall stateType actionType
-   . Ref stateType
-  -> Listeners stateType
-  -> RunReducers actionType stateType
-  -> actionType
-  -> Effect Unit
-update stateRef listeners reducers action = do
-  oldState <- read stateRef
-  newState <- reducers (\_ -> pure unit) oldState action
-  _ <- traverse (\f -> f newState) listeners
-  write newState stateRef
-
-getState
-  :: forall stateType 
-   . Ref stateType
-  -> Effect stateType
-getState stateRef = read stateRef
 
 createAndUse :: Effect Unit
 createAndUse = do 
@@ -130,33 +79,75 @@ createAndUse = do
 listener :: State -> Effect Unit
 listener = logShow
 
-{-
--- try out our Redux thing
--- see how we can use 
-tryLogin :: Effect State
-tryLogin 
-  = processAll defaultState actions
-  where
-    actions
-      = [ liftAction' Logout
-        , liftAction' (StartLogin "poo" "woo")
-        , liftAction' LoginSuccess
-        , liftAction' Up
-        , liftAction' Up
-        , liftAction' Down        
-        ]
-      -}
+
+type Reducer allActionType actionType stateType
+  =  (allActionType -> Effect Unit)
+  -> actionType
+  -> stateType
+  -> Effect stateType
+
+type CombinedReducer actionType stateType
+  =  (actionType -> Effect Unit)
+  -> stateType
+  -> actionType
+  -> Effect stateType
+
+type Listeners stateType
+  = Array (stateType -> Effect Unit)
+
+type Dispatcher actionType
+  = actionType -> Effect Unit
+
+data Store actionType stateType
+  = Store { dispatch :: Dispatcher actionType
+          , getState :: Effect stateType
+          }
 
 class HasLabel a (p :: Symbol) | a -> p 
 
-instance hasLabelLogin :: HasLabel Login "login" 
-instance hasLabelCounting :: HasLabel Counting "counting"
+createStore 
+  :: forall stateType actionType
+   . stateType
+  -> Listeners stateType
+  -> CombinedReducer actionType stateType
+  -> Effect (Store actionType stateType)
+createStore state listeners reducers = do
+  stateRef <- new state
+  pure $ Store { dispatch: (update stateRef listeners reducers)
+               , getState: (getState stateRef)
+               }
+  
+update 
+  :: forall stateType actionType
+   . Ref stateType
+  -> Listeners stateType
+  -> CombinedReducer actionType stateType
+  -> actionType
+  -> Effect Unit
+update stateRef listeners reducers action = do
+  -- read current state
+  oldState <- read stateRef
+  -- create a dispatcher
+  let dispatch = update stateRef listeners reducers
+  -- calculate new state
+  newState <- reducers dispatch oldState action
+  -- announce new state to listeners
+  _ <- traverse (\f -> f newState) listeners
+  -- save new state
+  write newState stateRef
+
+getState
+  :: forall stateType 
+   . Ref stateType
+  -> Effect stateType
+getState stateRef = read stateRef
 
 liftAction' 
-  :: forall label action dunno r. Cons label action dunno r 
-   => HasLabel action label 
-   => IsSymbol label 
-   => action 
-   -> Variant r
+  :: forall label action dunno r. 
+  Cons label action dunno r 
+  => HasLabel action label 
+  => IsSymbol label 
+  => action 
+  -> Variant r
 liftAction'  
   = inj (SProxy :: SProxy label) 
