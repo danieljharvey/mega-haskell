a044d9cefd3876720ccf93af4462259754e73227
Daniel Harvey <danieljamesharvey@gmail.com>
2019-08-09 11:10:07 +0100

    Yeah

--- purescript/src/FoldEvents.purs
+++ purescript/src/FoldEvents.purs
@@ -4,69 +4,33 @@ import Prelude
  
 import Data.Array (find) 
 import Data.Foldable (foldMap, foldr) 
-import Data.Lens (Prism', over, preview, prism', review) 
-import Data.Lens.Getter (to)
-import Data.Lens.Iso.Newtype (_Newtype)
-import Data.Lens.Internal.Forget (Forget)
+import Data.Lens (Getter', Lens', over, preview, view) 
+import Data.Lens.Getter (to) 
+import Data.Lens.Prism (Prism', prism', review)
 import Data.Lens.Record (prop)
-import Data.Maybe (Maybe(..), isJust)
+import Data.Maybe (Maybe(..), fromMaybe, isJust) 
 import Data.Maybe.Last (Last(..))
-import Data.Newtype (class Newtype, unwrap) 
-import Data.Profunctor (class Profunctor)
-import Data.Profunctor.Strong (class Strong)
+import Data.Newtype (unwrap) 
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 import Data.Symbol (SProxy(..))
 
 ---
 
-newtype PrismControl rawType niceType
-  = PrismControl
-      { events :: ControlEvents rawType
-      , to     :: niceType -> rawType
-      , from   :: rawType -> Maybe niceType
-      }
-
-derive instance newtypePrismControl 
-  :: Newtype (PrismControl i a) _
+type PrismControl rawType niceType
+  = { events    :: ControlEvents rawType
+    , to        :: niceType -> rawType
+    , maybeFrom :: rawType -> Maybe niceType
+    }
 
 ---
 
-newtype PrismDefaultControl rawType niceType
-  = PrismDefaultControl
-      { events  :: ControlEvents rawType
-      , to      :: niceType -> rawType
-      , from    :: rawType -> Maybe niceType
-      , default :: niceType
-      } 
-
-derive instance newtypePrismDefaultControl 
-  :: Newtype (PrismDefaultControl i a) _
-
----
-
-newtype LensControl rawType niceType
-  = LensControl
-      { events  :: ControlEvents rawType
-      , to      :: niceType -> rawType
-      , from    :: rawType -> niceType
-      }
-
-derive instance newtypeLensControl 
-  :: Newtype (LensControl i a) _
-
----
-
-newtype LensDefaultControl rawType niceType
-  = LensDefaultControl
-      { events  :: ControlEvents rawType
-      , to      :: niceType -> rawType
-      , from    :: rawType -> niceType
-      , default :: niceType
-      }
-
-derive instance newtypeLensDefaultControl 
-  :: Newtype (LensDefaultControl i a) _
+type PrismDefaultControl rawType niceType
+  = { events    :: ControlEvents rawType
+    , to        :: niceType -> rawType
+    , maybeFrom :: rawType -> Maybe niceType
+    , default   :: niceType
+    } 
 
 ---
 
@@ -76,97 +40,40 @@ type ControlEvents rawType
 ---
 
 events' 
-  :: forall i p rawType r
-   . Newtype i
-      { events :: ControlEvents rawType
-      | r
-      }
-  => Newtype i
-      { events :: ControlEvents rawType
-      | r
-      }
-  => Profunctor p 
-  => Strong p 
-  => p (ControlEvents rawType) (ControlEvents rawType) 
-  -> p i i
-events' = _Newtype <<< prop (SProxy :: SProxy "events")
+  :: forall rawType r
+   . Lens' { events :: ControlEvents rawType | r } (ControlEvents rawType)
+events' = prop (SProxy :: SProxy "events")
 
 ---
 
 to' 
-  :: forall i p r a
-   . Newtype i
-      { to :: a
-      | r
-      }
-  => Newtype i
-      { to :: a
-      | r
-      }
-  => Profunctor p 
-  => Strong p 
-  => p a a 
-  -> p i i
-to' = _Newtype <<< prop (SProxy :: SProxy "to")
+  :: forall niceType rawType r
+   . Lens' { to :: (niceType -> rawType) | r } (niceType -> rawType)
+to' = prop (SProxy :: SProxy "to")
 
 ---
 
 from' 
-  :: forall i p r a
-   . Newtype i
-      { from :: a
-      | r
-      }
-  => Newtype i
-      { from :: a
-      | r
-      }
-  => Profunctor p 
-  => Strong p 
-  => p a a 
-  -> p i i
-from' = _Newtype <<< prop (SProxy :: SProxy "from")
+  :: forall niceType rawType r
+   . Lens' { from :: (rawType -> niceType) | r } (rawType -> niceType)
+from' = prop (SProxy :: SProxy "from")
 
 ---
 
-default' 
-  :: forall i p r a
-   . Newtype i
-      { default :: a
-      | r
-      }
-  => Newtype i
-      { default :: a
-      | r
-      }
-  => Profunctor p 
-  => Strong p 
-  => p a a 
-  -> p i i
-default' = _Newtype <<< prop (SProxy :: SProxy "default")
+maybeFrom'
+  :: forall niceType rawType r
+   . Lens' { maybeFrom :: (rawType -> Maybe niceType) | r } (rawType -> Maybe niceType)
+maybeFrom'
+  = prop (SProxy :: SProxy "maybeFrom")
+---
+
+default'
+  :: forall niceType r
+   . Lens' { default :: niceType | r } niceType
+default' = prop (SProxy :: SProxy "default")
 
 --
 
-class WithEvents a rawType where
-  addEvent :: a -> Event rawType -> a
-
-instance withEventsPrismControl :: WithEvents (PrismControl rawType a) rawType where
-  addEvent prismControl event
-    = over events' (\as -> as <> [event]) prismControl
-
-instance withEventsPrismDefaultControl :: WithEvents (PrismDefaultControl rawType a) rawType where
-  addEvent prismDefaultControl event
-    = over events' (\as -> as <> [event]) prismDefaultControl
-
-instance withEventsLensControl :: WithEvents (LensControl rawType a) rawType where
-  addEvent lensControl event
-    = over events' (\as -> as <> [event]) lensControl
-
-instance withEventsLensDefaultControl :: WithEvents (LensDefaultControl rawType a) rawType where
-  addEvent lensDefaultControl event
-    = over events' (\as -> as <> [event]) lensDefaultControl
-
----
 
 data Pet
   = Dog
@@ -185,26 +92,39 @@ data Event rawType
 
 derive instance eqEvent :: (Eq internalType) => Eq (Event internalType)
 
-hasFocus 
-  :: forall i 
-   . ControlEvents i 
-  -> Boolean
-hasFocus events 
-  = foldr reducey false events
-  where
-    reducey
-      = (\e t -> case e of
-          OnFocus -> true
-          OnBlur  -> false
-          _       -> t) 
+---
 
-hasFocus' :: forall t173 t174 t176 t177 t182.
-  Newtype t176
-    { events :: Array (Event t174)
-    | t173
-    }
-   => Forget t182 Boolean t177 -> Forget t182 t176 t176
+hasFocus' 
+  :: forall rawType r
+   . Getter' { events :: ControlEvents rawType | r } Boolean
 hasFocus' = events' <<< to (hasFocus)
+  where
+    hasFocus 
+      = foldr reducey false 
+      where
+        reducey
+          = (\e t -> case e of
+              OnFocus -> true
+              OnBlur  -> false
+              _       -> t) 
+
+---
+
+hasBlurred' 
+  :: forall rawType r
+   . Eq rawType
+  => Getter' { events :: ControlEvents rawType | r } Boolean
+hasBlurred' = events' <<< to (isJust <<< find ((==) OnBlur))
+
+---
+
+hasHadFocus' 
+  :: forall rawType r
+   . Eq rawType
+  => Getter' { events :: ControlEvents rawType | r } Boolean
+hasHadFocus' = events' <<< to (isJust <<< find ((==) OnFocus))
+
+---
 
 mostRecentValue 
   :: forall i 
@@ -218,56 +138,99 @@ mostRecentValue events
           OnChange a -> Just a
           _          -> Nothing
 
-mostRecentValue' :: forall t187 t188 t190 t191 t196.
-  Newtype t190
-    { events :: Array (Event t188)
-    | t187
-    }
-   => Forget t196 (Maybe t188) t191 -> Forget t196 t190 t190
+mostRecentValue'
+  :: forall rawType r
+   . Getter' { events :: ControlEvents rawType | r} (Maybe rawType) 
 mostRecentValue'
   = events' <<< to (mostRecentValue)
 
-getOutput
+---
+
+getMaybeOutput
   :: forall i a
    . (i -> Maybe a)
   -> ControlEvents i
   -> Maybe a
-getOutput maybeFrom events
+getMaybeOutput maybeFrom events
   = mostRecentValue events >>= maybeFrom
 
+getMaybeOutput'
+  :: forall rawType niceType r
+   . Getter' { events :: ControlEvents rawType
+             , maybeFrom :: (rawType -> Maybe niceType) 
+             | r
+             }
+     (Maybe niceType) 
+getMaybeOutput'
+  = to outputGetter
+  where
+    outputGetter a
+      = getMaybeOutput (view maybeFrom' a) (view events' a)
 
+---
 
-outputIsValid
+maybeOutputIsValid
   :: forall i a 
    . (i -> Maybe a) 
   -> ControlEvents i
   -> Boolean
-outputIsValid maybeFrom events
-  = isJust (getOutput maybeFrom events)
+maybeOutputIsValid maybeFrom events
+  = isJust (getMaybeOutput maybeFrom events)
 
-hasBlurred 
-  :: forall i
-   . (Eq i) 
-  => ControlEvents i
-  -> Boolean
-hasBlurred events
-  = isJust (find ((==) OnBlur) events)
+maybeOutputIsValid' 
+  :: forall rawType niceType r
+   . Getter' { events :: ControlEvents rawType 
+             , maybeFrom :: (rawType -> Maybe niceType)
+             | r 
+             } Boolean
+maybeOutputIsValid' 
+  = to isValid
+  where
+    isValid a
+      = maybeOutputIsValid (view maybeFrom' a) (view events' a)
 
-hasHadFocus 
-  :: forall i 
-   . (Eq i) 
-  => ControlEvents i 
-  -> Boolean
-hasHadFocus events
-  = isJust (find ((==) OnFocus) events)
+---
 
-petSelector :: PrismControl String Pet
-petSelector 
-  = PrismControl
-      { events:  []
-      , from:    preview pet'
-      , to:      review pet'
-      }
+getOutput'
+  :: forall rawType niceType r
+   . Getter' { events    :: ControlEvents rawType
+             , maybeFrom :: (rawType -> Maybe niceType)
+             , default   :: niceType
+             | r
+             } niceType
+getOutput'
+  = to getOutput
+  where
+    getOutput a
+      = fromMaybe (view default' a) 
+                  (getMaybeOutput (view maybeFrom' a) (view events' a))
+
+---
+
+createEmpty 
+  :: forall rawType niceType
+   . Prism' rawType niceType
+  -> PrismControl rawType niceType
+createEmpty prism''
+  = { events: []
+    , maybeFrom: preview prism''
+    , to:        review prism''
+    }
+
+createWithDefault
+  :: forall rawType niceType
+   . Prism' rawType niceType
+  -> niceType
+  -> PrismDefaultControl rawType niceType
+createWithDefault prism'' def
+  = { events: []
+    , maybeFrom: preview prism''
+    , to:        review prism''
+    , default:   def
+    }
+
+
+---- sample
 
 pet' :: Prism' String Pet
 pet' = prism' toPet fromPet
@@ -278,3 +241,68 @@ pet' = prism' toPet fromPet
                  "Horse" -> Just Horse
                  _       -> Nothing
      toPet   = show
+
+
+petSelector :: PrismControl String Pet
+petSelector = createEmpty pet'
+
+
+log 
+  :: forall rawType r
+   . Event rawType
+  -> { events :: ControlEvents rawType | r }
+  -> { events :: ControlEvents rawType | r }
+log a 
+  = over events' (addEvent a)
+
+addEvent :: forall a. a -> Array a -> Array a
+addEvent a as 
+  = (as <> (pure a))
+
+testing 
+  :: forall r
+   . { events :: ControlEvents String | r }
+  -> { events :: ControlEvents String | r }
+testing  = (log OnFocus)
+       <<< (log OnBlur)
+       <<< (log (OnChange "oh"))
+       <<< (log (OnChange "Dog"))
+       <<< (log (OnChange "Doge"))
+
+value :: PrismControl String Pet
+value = testing (createEmpty pet')
+
+mostRecent :: Maybe Pet
+mostRecent = view getMaybeOutput' value
+-- Nothing 
+
+isFocused :: Boolean
+isFocused = view hasFocus' value
+-- false
+
+type TestRecord
+  = { val :: PrismControl String Pet
+    }
+
+testRecord :: TestRecord
+testRecord = { val: value }
+
+_val :: Lens' TestRecord (PrismControl String Pet)
+_val = prop (SProxy :: SProxy "val")
+
+valIsFocused 
+  :: TestRecord 
+  -> Boolean
+valIsFocused = view (_val <<< hasFocus') 
+
+logWithVal :: TestRecord
+logWithVal = over (_val <<< events') (addEvent OnFocus) testRecord
+
+no :: Boolean
+no = valIsFocused testRecord
+-- false
+
+yes :: Boolean
+yes = valIsFocused logWithVal
+-- true
+
