dd98c245f9d1e1914ee2c95de08b652c46b0ef46
Daniel Harvey <danieljamesharvey@gmail.com>
2019-08-31 23:38:42 +0100

    Rename to MiniStore

--- purescript/src/FoldEvents.purs
+++ purescript/src/FoldEvents.purs
@@ -1,181 +1,236 @@
 module FoldEvents where
 
 import Prelude
- 
-import Data.Array (find) 
-import Data.Array.NonEmpty (NonEmptyArray, last, singleton, snoc)
-import Data.Either (Either(..)) 
-import Data.Foldable (foldMap, foldl) 
-import Data.Lens (Lens', over, view) 
- 
+import Data.Array (find, length)
+import Data.Array.NonEmpty (NonEmptyArray, toArray, last, singleton, snoc)
+import Data.Either (Either(..))
+import Data.Foldable (foldMap, foldl, foldr)
+import Data.Lens (Lens', over, view)
 import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Lens.Record (prop)
-import Data.Maybe (Maybe(..), isJust) 
+import Data.Maybe (Maybe(..), isJust)
 import Data.Maybe.Last (Last(..))
-import Data.Newtype (class Newtype, unwrap) 
+import Data.Newtype (class Newtype, unwrap)
 import Data.Symbol (SProxy(..))
 
 ---
+newtype MiniStore rawType niceType err
+  = MiniStore (MiniStore' rawType niceType err)
 
-newtype PrismControl rawType niceType err
-  = PrismControl (PrismControl' rawType niceType err)
-
-type PrismControl' rawType niceType err
-  = { events     :: Array Event
-    , values     :: NonEmptyArray rawType
-    , to         :: niceType -> rawType
+type MiniStore' rawType niceType err
+  = { events :: Array Event
+    , values :: NonEmptyArray rawType
+    , to :: niceType -> rawType
     , eitherFrom :: rawType -> Either err niceType
     }
 
-derive instance newtypePrismControl :: Newtype (PrismControl a b c) _
+derive instance newtypeMiniStore :: Newtype (MiniStore a b c) _
 
 ---
-
-_events 
-  :: forall rawType a e
-   . Lens' (PrismControl rawType a e) (Array Event)
-_events 
-  =   _Newtype 
-  <<< prop (SProxy :: SProxy "events")
-
----
-
-_values 
-  :: forall rawType a e
-   . Lens' (PrismControl rawType a e) (NonEmptyArray rawType)
-_values 
-  =   _Newtype 
-  <<< prop (SProxy :: SProxy "values")
-
+_events ::
+  forall rawType a e.
+  Lens' (MiniStore rawType a e) (Array Event)
+_events =
+  _Newtype
+    <<< prop (SProxy :: SProxy "events")
 
 ---
-
-_to 
-  :: forall niceType rawType e
-   . Lens' (PrismControl rawType niceType e) (niceType -> rawType)
-_to 
-  =   _Newtype
-  <<< prop (SProxy :: SProxy "to")
+_values ::
+  forall rawType a e.
+  Lens' (MiniStore rawType a e) (NonEmptyArray rawType)
+_values =
+  _Newtype
+    <<< prop (SProxy :: SProxy "values")
 
 ---
+_to ::
+  forall niceType rawType e.
+  Lens' (MiniStore rawType niceType e) (niceType -> rawType)
+_to =
+  _Newtype
+    <<< prop (SProxy :: SProxy "to")
 
-_eitherFrom 
-  :: forall niceType rawType e
-   . Lens' (PrismControl rawType niceType e) (rawType -> Either e niceType)
-_eitherFrom 
-  =   _Newtype
-  <<< prop (SProxy :: SProxy "eitherFrom")
+---
+_eitherFrom ::
+  forall niceType rawType e.
+  Lens' (MiniStore rawType niceType e) (rawType -> Either e niceType)
+_eitherFrom =
+  _Newtype
+    <<< prop (SProxy :: SProxy "eitherFrom")
 
 --
-
-data Event 
+data Event
   = OnBlur
   | OnFocus
+  | OnSubmit
+  | OnClick
 
 derive instance eqEvent :: Eq Event
+
 instance showEvent :: Show Event where
   show OnBlur = "OnBlur"
   show OnFocus = "OnFocus"
+  show OnSubmit = "OnSubmit"
+  show OnClick = "OnClick"
 
 ---
-
-hasFocus 
-  :: forall rawType niceType e
-   . PrismControl rawType niceType e
-  -> Boolean
-hasFocus a 
-  = foldl hasFocusFold false (view _events a)
+hasFocus ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  Boolean
+hasFocus a = foldl hasFocusFold false (view _events a)
 
 hasFocusFold :: Boolean -> Event -> Boolean
-hasFocusFold _ value
-  = case value of
-         OnFocus -> true
-         OnBlur  -> false
+hasFocusFold current value = case value of
+  OnFocus -> true
+  OnBlur -> false
+  _ -> current
 
 ---
-
-hasBlurred 
-  :: forall rawType niceType e
-   . PrismControl rawType niceType e
-  -> Boolean
-hasBlurred 
-  = isJust <<< find ((==) OnBlur) <<< view _events
+hasBlurred ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  Boolean
+hasBlurred = hasEvent OnBlur
 
 ---
-
-hasHadFocus 
-  :: forall rawType niceType e 
-   . PrismControl rawType niceType e
-  -> Boolean
-hasHadFocus 
-  = isJust <<< find ((==) OnFocus) <<< view _events
+hasHadFocus ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  Boolean
+hasHadFocus = hasEvent OnFocus
 
 ---
-
-mostRecentValue 
-  :: forall rawType niceType e 
-   . PrismControl rawType niceType e
-  -> rawType
-mostRecentValue = last <<< view _values
+hasSucceeded ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  Boolean
+hasSucceeded =
+  (_ > 0)
+    <<< length
+    <<< allGoodValues
 
 ---
+hasSubmitted ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  Boolean
+hasSubmitted = hasEvent OnSubmit
 
-lastGoodValue
-  :: forall rawType niceType e
-   . PrismControl rawType niceType e
-  -> Maybe niceType
-lastGoodValue a
-  = unwrap $ foldMap Last convertedValues
+hasBeenClicked ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  Boolean
+hasBeenClicked = hasEvent OnClick
 
+hasEvent ::
+  forall rawType niceType e.
+  Event ->
+  MiniStore rawType niceType e ->
+  Boolean
+hasEvent event =
+  isJust
+    <<< find ((==) event)
+    <<< view _events
+
+---
+mostRecentValue ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  rawType
+mostRecentValue =
+  last
+    <<< view _values
+
+---
+lastGoodValue ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  Maybe niceType
+lastGoodValue a = unwrap $ foldMap Last convertedValues
   where
-    convertedValues
-      = map (eitherToMaybe <<< (view _eitherFrom a)) (view _values a)
-    
-    eitherToMaybe
-      = case _ of
-          Right a' -> Just a'          
-          _        -> Nothing
----
+  convertedValues =
+    map
+      ( eitherToMaybe
+          <<< (view _eitherFrom a)
+      )
+      (view _values a)
 
+  eitherToMaybe = case _ of
+    Right a' -> Just a'
+    _ -> Nothing
+
+---
+allValues ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  NonEmptyArray rawType
+allValues = view _values
+
+---
+allGoodValues ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  Array niceType
+allGoodValues a =
+  ( filterRight
+      <<< toArray
+      <<< map (view _eitherFrom a)
+      <<< allValues
+  )
+    a
+
+---
+allGoodValuesRaw ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e -> Array rawType
+allGoodValuesRaw a = map (view _to a) (allGoodValues a)
+
+---
+filterRight :: forall e a. Array (Either e a) -> Array a
+filterRight = foldr foldFunc mempty
+  where
+  foldFunc item total = case item of
+    Right a -> total <> pure a
+    _ -> total
+
+---
 -- | Get the current value and check if it is valid
-currentValue
-  :: forall rawType niceType e
-   . PrismControl rawType niceType e
-  -> Either e niceType
+currentValue ::
+  forall rawType niceType e.
+  MiniStore rawType niceType e ->
+  Either e niceType
 currentValue a = view _eitherFrom a (mostRecentValue a)
 
--- | External API - create a new PrismControl with getter/setters
+-- | External API - create a new MiniStore with getter/setters
 -- | and a default rawValue
-createEmpty 
-  :: forall rawType niceType e
-   . rawType
-  -> (niceType -> rawType)
-  -> (rawType -> Either e niceType)
-  -> PrismControl rawType niceType e
-createEmpty default to from
-  = PrismControl
-      { events:    mempty
-      , values: singleton default
-      , eitherFrom: from
-      , to:        to
-      }
+createEmpty ::
+  forall rawType niceType e.
+  rawType ->
+  (niceType -> rawType) ->
+  (rawType -> Either e niceType) ->
+  MiniStore rawType niceType e
+createEmpty default to from =
+  MiniStore
+    { events: mempty
+    , values: singleton default
+    , eitherFrom: from
+    , to: to
+    }
 
 -- | Log a predefined DOM event
 -- | Ie, onBlur, onFocus, etc
-logEvent 
-  :: forall rawType niceType e
-   . Event
-  -> PrismControl rawType niceType e
-  -> PrismControl rawType niceType e
-logEvent a 
-  = over _events (\as -> (as <> (pure a)))
+logEvent ::
+  forall rawType niceType e.
+  Event ->
+  MiniStore rawType niceType e ->
+  MiniStore rawType niceType e
+logEvent a = over _events (\as -> (as <> (pure a)))
 
 -- | Log a change in value
-logValue
-  :: forall rawType niceType e
-   . rawType
-  -> PrismControl rawType niceType e
-  -> PrismControl rawType niceType e
-logValue a
-  = over _values (flip snoc a)
-
+logValue ::
+  forall rawType niceType e.
+  rawType ->
+  MiniStore rawType niceType e ->
+  MiniStore rawType niceType e
+logValue a = over _values (flip snoc a)
--- purescript/test/FoldEvents.purs
+++ purescript/test/FoldEvents.purs
@@ -6,13 +6,9 @@ import Data.Maybe (Maybe(..))
 import Control.Monad.Free (Free)
 import Test.Unit (suite, test, TestF)
 import Test.Unit.Assert as Assert
-import Data.Lens (Lens', over, view) 
-import Data.Lens.Record (prop)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
-import Data.Symbol (SProxy(..))
-
-import FoldEvents (Event(..), PrismControl, createEmpty, currentValue, hasFocus, lastGoodValue, logEvent, logValue)
+import FoldEvents (Event(..), MiniStore, allGoodValues, createEmpty, currentValue, hasFocus, lastGoodValue, logEvent, logValue)
 
 tests :: Free TestF Unit
 tests = do
@@ -20,23 +16,23 @@ tests = do
     test "Returns Nothing when no most recent value" do
       Assert.equal (currentValue petValue) (Left (UnknownPet "oh"))
     test "Returns Just Dog when no most recent value" do
-       let petWithValue = logValue "Dog" petValue 
-       Assert.equal (currentValue petWithValue) (Right Dog)
+      let
+        petWithValue = logValue "Dog" petValue
+      Assert.equal (currentValue petWithValue) (Right Dog)
     test "Finds last good value" do
-       Assert.equal (lastGoodValue petValue) (Just Dog)
+      Assert.equal (lastGoodValue petValue) (Just Dog)
     test "Knows that we are not currently focused" do
-       Assert.equal (hasFocus petValue) false
-    test "Works nested in a record" do
-       Assert.equal (hasFocus $ view _val testRecord) false
-    test "Updates work nested in a record" do
-       Assert.equal (hasFocus $ view _val testRecordWithVal) true
+      Assert.equal (hasFocus petValue) false
+    test "Gets allGoodValues" do
+      Assert.equal [ Cat, Dog ] (allGoodValues (logValue "Cat" petValue))
 
 data Pet
   = Dog
   | Cat
   | Horse
 
-derive instance genericPet :: Generic Pet _ 
+derive instance genericPet :: Generic Pet _
+
 derive instance eqPet :: Eq Pet
 
 instance showPet :: Show Pet where
@@ -46,60 +42,30 @@ data PetError
   = UnknownPet String
 
 derive instance eqPetError :: Eq PetError
+
 instance showPetError :: Show PetError where
   show (UnknownPet s) = "Unknown Pet: " <> s
 
-
 fromPet :: String -> Either PetError Pet
-fromPet = case _ of 
-   "Dog"   -> Right Dog
-   "Cat"   -> Right Cat
-   "Horse" -> Right Horse
-   a       -> Left (UnknownPet a)
+fromPet = case _ of
+  "Dog" -> Right Dog
+  "Cat" -> Right Cat
+  "Horse" -> Right Horse
+  a -> Left (UnknownPet a)
 
 toPet :: Pet -> String
-toPet   = show
+toPet = show
 
 ---
+addTestValues ::
+  MiniStore String Pet PetError ->
+  MiniStore String Pet PetError
+addTestValues =
+  (logEvent OnBlur)
+    <<< (logEvent OnFocus)
+    <<< (logValue "oh")
+    <<< (logValue "Dog")
+    <<< (logValue "Doge")
 
-
-addTestValues 
-  :: PrismControl String Pet PetError
-  -> PrismControl String Pet PetError
-addTestValues  
-  = (logEvent OnBlur)
-       <<< (logEvent OnFocus)
-       <<< (logValue "oh")
-       <<< (logValue "Dog")
-       <<< (logValue "Doge")
-
-petValue :: PrismControl String Pet PetError
+petValue :: MiniStore String Pet PetError
 petValue = addTestValues (createEmpty "" toPet fromPet)
-
-type TestRecord
-  = { val  :: PrismControl String Pet PetError
-    , val2 :: PrismControl String Pet PetError
-    }
-
-testRecord :: TestRecord
-testRecord 
-  = { val:  createEmpty "" toPet fromPet
-    , val2: createEmpty "" toPet fromPet
-    }
-
-_val :: Lens' TestRecord (PrismControl String Pet PetError)
-_val = prop (SProxy :: SProxy "val")
-
-_val2 :: Lens' TestRecord (PrismControl String Pet PetError)
-_val2 = prop (SProxy :: SProxy "val2")
-
-valIsFocused 
-  :: TestRecord 
-  -> Boolean
-valIsFocused = hasFocus <<< view _val
-
-testRecordWithVal :: TestRecord
-testRecordWithVal 
-  = over _val (logEvent OnFocus) testRecord
-
-
